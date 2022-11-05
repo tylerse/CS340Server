ef616a60bf5f70d0bce36877c8787f41251f3895
tylerse <71523039+tylerse@users.noreply.github.com>
2022-11-04 18:31:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express, { response } from 'express';
 import * as sql from './SqlHandler.js'
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 3030;
 
 app.use(express.json())
 app.use(express.urlencoded({
